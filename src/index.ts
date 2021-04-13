47c6fb2eac27f3e602635e854b600b9b416d7f5e
Brian Anderson <brian@getnacelle.com>
2021-04-13 12:02:19 -0500

    updated port

--- src/index.ts
+++ src/index.ts
@@ -695,5 +695,5 @@ app.delete('/variant-metafields/:id', async (req, res) => {
     });
   }
 });
-
-app.listen(3000, () => console.log('REST API server ready at: http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('REST API server ready at: http://localhost:3000'));
