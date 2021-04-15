a932587e3d477f3e4943206c145896435a8ed249
Brian Anderson <brian@getnacelle.com>
2021-04-15 17:40:31 -0500

    added variant metafields and webhook config routers

---
+++ src/api/v1/variant-metafields.ts
@@ -0,0 +1,100 @@
+import { PrismaClient } from '@prisma/client';
+import express from 'express';
+import { EventEmitter } from 'events';
+import { WebhookEmitter } from '../../webhook-emitter';
+
+const variantMetafieldsRouter = express.Router();
+const prisma = new PrismaClient();
+const emitter = new EventEmitter();
+
+variantMetafieldsRouter.post('/', async (req, res) => {
+  const data = { ...req.body };
+  try {
+    const variantMetafield = await prisma.variantMetafield.create({
+      data,
+    });
+    res.json(variantMetafield);
+  } catch (error) {
+    res.json({
+      code: String(error.code),
+      meta: error.meta,
+      message: String(error.message),
+    });
+  }
+});
+
+variantMetafieldsRouter.get('/', async (req, res) => {
+  const { skip, take } = req.query;
+  try {
+    const variantMetafields = await prisma.variantMetafield.findMany({
+      skip: skip ? Number(skip) : 0,
+      take: take ? Number(take) : 25,
+    });
+    res.json(variantMetafields);
+  } catch (error) {
+    res.json({
+      code: String(error.code),
+      meta: error.meta,
+      message: String(error.message),
+    });
+  }
+});
+
+variantMetafieldsRouter.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const variantMetafield = await prisma.variantMetafield.findUnique({
+      where: { id: Number(id) },
+    });
+    if (variantMetafield === null) {
+      res.sendStatus(404);
+    } else {
+      res.json(variantMetafield);
+    }
+  } catch (error) {
+    res.json({
+      code: String(error.code),
+      meta: error.meta,
+      message: String(error.message),
+    });
+  }
+});
+
+variantMetafieldsRouter.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const data = { ...req.body };
+  try {
+    const variantMetafield = await prisma.variantMetafield.update({
+      where: { id: Number(id) },
+      data,
+    });
+    res.json(variantMetafield);
+  } catch (error) {
+    res.json({
+      code: String(error.code),
+      meta: error.meta,
+      message: String(error.message),
+    });
+  }
+});
+
+variantMetafieldsRouter.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const variantMetafield = await prisma.variantMetafield.delete({
+      where: { id: Number(id) },
+    });
+    res.json({
+      message: 'DELETE successful',
+      variantMetafield,
+    });
+  } catch (error) {
+    res.json({
+      code: String(error.code),
+      meta: error.meta,
+      message: String(error.message),
+    });
+  }
+});
+
+export default variantMetafieldsRouter;
---
+++ src/api/v1/webhook-configs.ts
@@ -0,0 +1,110 @@
+import { PrismaClient } from '@prisma/client';
+import express from 'express';
+import { EventEmitter } from 'events';
+import { WebhookEmitter } from '../../webhook-emitter';
+
+const webhookConfigRouter = express.Router();
+const prisma = new PrismaClient();
+const emitter = new EventEmitter();
+
+webhookConfigRouter.post('/', async (req, res) => {
+  const data = { ...req.body };
+  try {
+    const webhookConfig = await prisma.webhookConfig.create({
+      data,
+    });
+    res.json(webhookConfig);
+  } catch (error) {
+    res.json({
+      code: String(error.code),
+      meta: error.meta,
+      message: String(error.message),
+    });
+  }
+});
+
+webhookConfigRouter.get('/', async (req, res) => {
+  const { skip, take } = req.params;
+  const { topic } = req.query;
+  try {
+    if (topic) {
+      const webhookConfigs = await prisma.webhookConfig.findMany({
+        skip: skip ? Number(skip) : 0,
+        take: take ? Number(take) : 25,
+        where: { topic: String(topic) },
+      });
+      res.json(webhookConfigs);
+    } else {
+      const webhookConfigs = await prisma.webhookConfig.findMany({
+        skip: skip ? Number(skip) : 0,
+        take: take ? Number(take) : 25,
+      });
+      res.json(webhookConfigs);
+    }
+  } catch (error) {
+    res.json({
+      code: String(error.code),
+      meta: error.meta,
+      message: String(error.message),
+    });
+  }
+});
+
+webhookConfigRouter.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const webhookConfig = await prisma.webhookConfig.findUnique({
+      where: { id: Number(id) },
+    });
+    if (webhookConfig === null) {
+      res.sendStatus(404);
+    } else {
+      res.json(webhookConfig);
+    }
+  } catch (error) {
+    res.json({
+      code: String(error.code),
+      meta: error.meta,
+      message: String(error.message),
+    });
+  }
+});
+
+webhookConfigRouter.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const data = { ...req.body };
+  try {
+    const webhookConfig = await prisma.webhookConfig.update({
+      where: { id: Number(id) },
+      data,
+    });
+    res.json(webhookConfig);
+  } catch (error) {
+    res.json({
+      code: String(error.code),
+      meta: error.meta,
+      message: String(error.message),
+    });
+  }
+});
+
+webhookConfigRouter.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const webhookConfig = await prisma.webhookConfig.delete({
+      where: { id: Number(id) },
+    });
+    res.json({
+      message: 'DELETE successful',
+      webhookConfig,
+    });
+  } catch (error) {
+    res.json({
+      code: String(error.code),
+      meta: error.meta,
+      message: String(error.message),
+    });
+  }
+});
+
+export default webhookConfigRouter;
