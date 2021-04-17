2e71c933fd9e692580c8e46e419d94b02dbb3e5a
Brian Anderson <brian@getnacelle.com>
2021-04-17 15:58:49 -0500

    removed productHandle from VariantContent in schema

---
+++ prisma/migrations/20210417205830_removed/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `productHandle` on the `VariantContent` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "VariantContent" DROP COLUMN "productHandle";
