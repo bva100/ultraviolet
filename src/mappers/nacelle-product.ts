46e0a75b8a1e38b15929b6a9ce1a4215e2ca351c
bva100 <brianvanderson@gmail.com>
2021-05-02 09:06:05 -0500

    added tests and properties to nacelle product mapper

--- src/mappers/nacelle-product.ts
+++ src/mappers/nacelle-product.ts
@@ -5,7 +5,15 @@ export class NacelleProduct extends NacelleBase {
 
   availableForSale : boolean;
 
-  // availableForSale: boolean
+  productType: string | null;
+
+  vendor: string | null;
+
+  tags: string[] | null;
+
+  createdAt: number | null;
+
+  updatedAt: number | null;
 
   constructor(params: any) {
     super(params);
@@ -20,6 +28,37 @@ export class NacelleProduct extends NacelleBase {
       throw new Error('Nacelle Product mapper must have the availableForSale parameter');
     } else {
       this.availableForSale = params.availableForSale;
+    }
+
+    if (params.productType) {
+      this.productType = params.productType;
+    } else {
+      this.productType = null;
+    }
+
+    if (params.vendor) {
+      this.vendor = params.vendor;
+    } else {
+      this.vendor = null;
+    }
+
+    if (params.tags) {
+      const tagArray = params.tags.split(',');
+      this.tags = tagArray.map((tag: string) => tag.trim());
+    } else {
+      this.tags = null;
+    }
+
+    if (params.createdAt) {
+      this.createdAt = Math.round(new Date(params.createdAt).getTime() / 1000);
+    } else {
+      this.createdAt = null;
+    }
+
+    if (params.updatedAt) {
+      this.updatedAt = Math.round(new Date(params.updatedAt).getTime() / 1000);
+    } else {
+      this.updatedAt = null;
     }
   }
 }
--- tests/src/mappers/nacelle-product.unit.test.ts
+++ tests/src/mappers/nacelle-product.unit.test.ts
@@ -23,8 +23,13 @@ test('Constructor throws if the availableForSale parameter does not exist', () =
 
 const params = {
   id: 'zyx123',
-  handle: 'foobar',
+  handle: 'chair',
   availableForSale: true,
+  productType: 'furniture',
+  vendor: 'Knoll',
+  tags: 'abc, 123, zyx',
+  createdAt: '2021-04-12T19:54:41.390Z',
+  updatedAt: '2021-04-12T19:58:24.840Z',
 };
 
 const nacelleProduct = new NacelleProduct(params);
@@ -39,4 +44,24 @@ test('Constructor sets handle', () => {
 
 test('Constructor sets availableForSale', () => {
   expect(nacelleProduct.availableForSale).toBe(params.availableForSale);
+});
+
+test('Constructor sets productType', () => {
+  expect(nacelleProduct.productType).toBe(params.productType);
+});
+
+test('Constructor sets vendor', () => {
+  expect(nacelleProduct.vendor).toBe(params.vendor);
+});
+
+test('Constructor sets and formats tags', () => {
+  expect(nacelleProduct.tags).toEqual(['abc', '123', 'zyx']);
+});
+
+test('Constructor sets and formats createdAt', () => {
+  expect(nacelleProduct.createdAt).toEqual(1618257281);
+});
+
+test('Constructor sets and formats updatedAt', () => {
+  expect(nacelleProduct.updatedAt).toEqual(1618257505);
 });
